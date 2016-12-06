96b9b6e12e0f47e896552072d2d48e972303e3d5
Caroline Trimble <carolinetrimble@dyn-160-39-170-218.dyn.columbia.edu>
2016-12-06 18:24:56 -0500

    other stuff

---
+++ script.js
@@ -0,0 +1,38 @@
+var type = "walk";
+var filters =[];
+
+function walk()
+{
+  type = "walk";
+  //sets the mode of transportation to walk
+}
+
+function bike()
+{
+  type="bike";
+  //sets the mode of transportation to bike
+}
+
+function barre(){
+  filters.push("barre");
+}
+
+function boxing(){
+  filters.push("boxing");
+}
+
+function yoga(){
+  filters.push("yoga");
+}
+
+function gym(){
+  filters.push("gym");
+}
+
+function pilates(){
+  filters.push("pilates");
+}
+
+function spin(){
+  filers.push("spin");
+}
