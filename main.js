0df7f556e4102de8fa65b98a56dfa2b5579f9e68
cassiewes <ciw2109@columbia.edu>
2016-12-06 19:47:47 -0500

    put javascript in js folder for main stuff

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
 var map;
-var service;
 var infowindow;
 
-function initialize() {
-  var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+function initMap() {
+  var locate = {lat: -33.867, lng: 151.195}; //locate holds the user's inputed address
 
   map = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
-      zoom: 15
-    });
+    center: locate,
+    zoom: 16
+  });
 
-  var request = {
-    location: pyrmont,
-    radius: '500',
-    query: 'restaurant'
-  };
-
-  service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, callback);
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: locate,
+    radius: 500,
+    type: ['store'] //input the user's types of excercises here
+  }, callback);
 }
 
 function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
-      var place = results[i];
       createMarker(results[i]);
     }
   }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
 }
