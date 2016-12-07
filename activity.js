722e7ec638cf77e445063a5d0e0c4d1c6941906a
cassiewes <ciw2109@columbia.edu>
2016-12-07 14:42:29 -0500

    activity design- highlight is not working in chrome, but works in IE

---
+++ activity.js
@@ -0,0 +1,24 @@
+function barre() {
+    document.getElementById("barre").style.border = "thick solid #67696d";
+    document.getElementById("barretext").style.color = "#67696d";
+}
+function boxing() {
+    document.getElementById("boxing").style.border = "thick solid #67696d";
+    document.getElementById("boxingtext").style.color = "#67696d";
+}
+function yoga() {
+    document.getElementById("yoga").style.border = "thick solid #67696d";
+    document.getElementById("yogatext").style.color = "#67696d";
+}
+function gym() {
+    document.getElementById("gym").style.border = "thick solid #67696d";
+    document.getElementById("gymtext").style.color = "#67696d";
+}
+function pilates() {
+    document.getElementById("pilates").style.border = "thick solid #67696d";
+    document.getElementById("pilatestext").style.color = "#67696d";
+}
+function spin() {
+    document.getElementById("spin").style.border = "thick solid #67696d";
+    document.getElementById("spintext").style.color = "#67696d";
+}
