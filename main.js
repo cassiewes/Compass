1726c4b8b450cfcfb0cecef359239b76b2fb525d
cassiewes <ciw2109@columbia.edu>
2016-12-06 20:45:03 -0500

    combined google directions api with google maps/places

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var map;
 var infowindow;
-
+/*
 function initMap() {
   var locate = {lat: 40.779598, lng: -73.977601}; //locate holds the user's inputed address
 
@@ -8,6 +8,7 @@ function initMap() {
     center: locate,
     zoom: 16
   });
+
 
   infowindow = new google.maps.InfoWindow();
   var service = new google.maps.places.PlacesService(map);
@@ -18,25 +19,136 @@ function initMap() {
     type: 'gym'
   }, callback);
 }
+*/
 
-function callback(results, status) {
-  console.log(results);
-  if (status === google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      createMarker(results[i]);
+//directions here
+function initMap() {
+
+  var locate = {lat: 40.779598, lng: -73.977601};
+    var origin_place_id = null;
+    var destination_place_id = null;
+    var travel_mode = 'WALKING';
+    var map = new google.maps.Map(document.getElementById('map'), {
+      mapTypeControl: false,
+      center: locate,
+      zoom: 16
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+      location: locate,
+      radius: 500,
+      keyword: 'pilate',
+      type: 'gym'
+    }, callback);
+
+    function callback(results, status) {
+      console.log(results);
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
+      }
+    }
+
+    function createMarker(place) {
+      var placeLoc = place.geometry.location;
+      var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+      });
+
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+      });
+    }
+
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    directionsDisplay.setMap(map);
+
+    var origin_input = document.getElementById('origin-input');
+    var destination_input = document.getElementById('destination-input');
+    var modes = document.getElementById('mode-selector');
+
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(origin_input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(destination_input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(modes);
+
+    var origin_autocomplete = new google.maps.places.Autocomplete(origin_input);
+    origin_autocomplete.bindTo('bounds', map);
+    var destination_autocomplete =
+        new google.maps.places.Autocomplete(destination_input);
+    destination_autocomplete.bindTo('bounds', map);
+
+    // Sets a listener on a radio button to change the filter type on Places
+    // Autocomplete.
+    function setupClickListener(id, mode) {
+      var radioButton = document.getElementById(id);
+      radioButton.addEventListener('click', function() {
+        travel_mode = mode;
+      });
+    }
+    setupClickListener('changemode-walking', 'WALKING');
+    setupClickListener('changemode-transit', 'TRANSIT');
+    setupClickListener('changemode-driving', 'DRIVING');
+
+    function expandViewportToFitPlace(map, place) {
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+      }
+    }
+
+    origin_autocomplete.addListener('place_changed', function() {
+      var place = origin_autocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+      }
+      expandViewportToFitPlace(map, place);
+
+      // If the place has a geometry, store its place ID and route if we have
+      // the other place ID
+      origin_place_id = place.place_id;
+      route(origin_place_id, destination_place_id, travel_mode,
+            directionsService, directionsDisplay);
+    });
+
+    destination_autocomplete.addListener('place_changed', function() {
+      var place = destination_autocomplete.getPlace();
+      if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+      }
+      expandViewportToFitPlace(map, place);
+
+      // If the place has a geometry, store its place ID and route if we have
+      // the other place ID
+      destination_place_id = place.place_id;
+      route(origin_place_id, destination_place_id, travel_mode,
+            directionsService, directionsDisplay);
+    });
+
+    function route(origin_place_id, destination_place_id, travel_mode,
+                   directionsService, directionsDisplay) {
+      if (!origin_place_id || !destination_place_id) {
+        return;
+      }
+      directionsService.route({
+        origin: {'placeId': origin_place_id},
+        destination: {'placeId': destination_place_id},
+        travelMode: travel_mode
+      }, function(response, status) {
+        if (status === 'OK') {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
     }
   }
-}
-
-function createMarker(place) {
-  var placeLoc = place.geometry.location;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
-  });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
-  });
-}
