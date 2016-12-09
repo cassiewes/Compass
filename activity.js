4dbbb313b02f774a1522ee23f48e3843db2bd4a8
cassiewes <ciw2109@columbia.edu>
2016-12-08 21:55:58 -0500

    button only appears when at least one activity is selected

--- activity.js
+++ activity.js
@@ -1,5 +1,6 @@
 var method ="";
 var keyword = "";
+
 function getTransit(){
   method = window.location.hash.substr(1);
 }
@@ -8,10 +9,12 @@ function addBarre() {
     if(document.getElementById("barre").style.border != "thick solid rgb(103, 105, 109)"){
       document.getElementById("barre").style.border = "thick solid #67696d";
       document.getElementById("barretext").style.color = "#67696d";
+      $('#next').css('visibility','visible');
     }
     else if(document.getElementById("barre").style.border == "thick solid rgb(103, 105, 109)"){
       document.getElementById("barre").style.border = "0";
       document.getElementById("barretext").style.color = "#eaedf2";
+      $('#next').css('visibility','hidden');
     }
     keyword = keyword+ "_barre";
     getTransit();
@@ -21,10 +24,12 @@ function addBoxing() {
   if(document.getElementById("boxing").style.border != "thick solid rgb(103, 105, 109)"){
     document.getElementById("boxing").style.border = "thick solid #67696d";
     document.getElementById("boxingtext").style.color = "#67696d";
+    $('#next').css('visibility','visible');
   }
   else if(document.getElementById("boxing").style.border == "thick solid rgb(103, 105, 109)"){
     document.getElementById("boxing").style.border = "0";
     document.getElementById("boxingtext").style.color = "#eaedf2";
+    $('#next').css('visibility','hidden');
   }
     keyword = keyword + "_boxing";
     getTransit()
@@ -33,10 +38,12 @@ function addYoga() {
   if(document.getElementById("yoga").style.border != "thick solid rgb(103, 105, 109)"){
     document.getElementById("yoga").style.border = "thick solid #67696d";
     document.getElementById("yogatext").style.color = "#67696d";
+    $('#next').css('visibility','visible');
   }
   else if(document.getElementById("yoga").style.border == "thick solid rgb(103, 105, 109)"){
     document.getElementById("yoga").style.border = "0";
     document.getElementById("yogatext").style.color = "#eaedf2";
+    $('#next').css('visibility','hidden');
   }
     keyword = keyword +"_yoga";
     getTransit();
@@ -45,10 +52,12 @@ function addGym() {
   if(document.getElementById("gym").style.border != "thick solid rgb(103, 105, 109)"){
     document.getElementById("gym").style.border = "thick solid #67696d";
     document.getElementById("gymtext").style.color = "#67696d";
+    $('#next').css('visibility','visible');
   }
   else if(document.getElementById("gym").style.border == "thick solid rgb(103, 105, 109)"){
     document.getElementById("gym").style.border = "0";
     document.getElementById("gymtext").style.color = "#eaedf2";
+    $('#next').css('visibility','hidden');
   }
     keyword = keyword + "_gym";
     getTransit()
@@ -57,10 +66,12 @@ function addPilates() {
   if(document.getElementById("pilates").style.border != "thick solid rgb(103, 105, 109)"){
     document.getElementById("pilates").style.border = "thick solid #67696d";
     document.getElementById("pilatestext").style.color = "#67696d";
+    $('#next').css('visibility','visible');
   }
   else if(document.getElementById("pilates").style.border == "thick solid rgb(103, 105, 109)"){
     document.getElementById("pilates").style.border = "0";
     document.getElementById("pilatestext").style.color = "#eaedf2";
+    $('#next').css('visibility','hidden');
   }
     keyword=keyword +"_pilates";
     getTransit()
@@ -69,10 +80,12 @@ function addSpin() {
   if(document.getElementById("spin").style.border != "thick solid rgb(103, 105, 109)"){
     document.getElementById("spin").style.border = "thick solid #67696d";
     document.getElementById("spintext").style.color = "#67696d";
+    $('#next').css('visibility','visible');
   }
   else if(document.getElementById("spin").style.border == "thick solid rgb(103, 105, 109)"){
     document.getElementById("spin").style.border = "0";
     document.getElementById("spintext").style.color = "#eaedf2";
+    $('#next').css('visibility','hidden');
   }
     keyword=keyword +"_spin";
     getTransit()
